af5960d8fd5af6e5d7bfaf20d0a09a23d0cbd07e
smakthe <scmakra99@gmail.com>
2024-12-03 16:51:18 +0530

    Changes for CORS

--- src/server.js
+++ src/server.js
@@ -1,22 +1,19 @@
 import express, {json} from 'express'
 import connectDB from './db/connection.js'
 import wordsRouter from './routes/words.js'
+import cors from 'cors'
 
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5000'
 const PORT = process.env.PORT || 5000
 
 const app = express();
 
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", FRONTEND_URL);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+app.use(cors(),json())
 
 app.use('/api', wordsRouter);
 
 connectDB()
 
-app.use(json()).listen(PORT,() => {
+app.listen(PORT,() => {
     console.log("Application listening on Port",PORT)
 })
